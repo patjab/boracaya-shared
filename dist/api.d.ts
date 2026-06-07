43cd35e8c817a60c00cd65aa4668ebe13347e240
Patrick Abejar <14173988+patjab@users.noreply.github.com>
2026-06-07 17:08:59 -0400

    chore: add dist/api.d.ts

---
+++ dist/api.d.ts
@@ -0,0 +1,17 @@
+export declare const ApiConstants: Readonly<{
+    GET_ALL_INVITES: string;
+    GET_INVITE: string;
+    CREATE_INVITES_BY_CSV_UPLOAD: string;
+    INCREMENT_COUNT_OF_INVITE_SENT: string;
+    GET_RSVPS: string;
+    GET_AND_PUT_ADMIN_ADDED_DATA_TO_RSVPS: string;
+    GET_PRECHECKINS: string;
+    GET_SAVE_THE_DATE_RECORDS: string;
+    GET_ALL_SURVEYS: string;
+    GET_SURVEY_COUNTS: string;
+    GET_IP_ADDRESSES: string;
+    LOGIN: string;
+    WISHES: string;
+    INITIATE_UPLOAD: string;
+    COMPLETE_UPLOAD: string;
+}>;
